6bae544d0ff9cb0ebaaea36c5f43abbd66b86a03
Onwunali Tochukwu <onwunali91@yahoo.com>
2024-06-14 10:02:37 +0100

    first hydro commit

---
+++ app/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Announcement() {
+  return (
+    <section className="announcement">
+        <div className="container">
+            <p>Just Hit Send: Get A Digital Gift Card For Dad (Easy, Foolproof, And Always Fits.)</p>
+            <a href="#">Shop Now</a>
+        </div>
+    </section>
+  )
+}
